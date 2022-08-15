7269eeaf23e17b75bc9348cd2a42d626a7b1f080
Caio Hatai <caio.haruo@hotmail.com>
2022-08-15 15:33:46 -0300

    initial commit

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -1,13 +1,15 @@
-import { Button } from "@chakra-ui/button";
+import { Button } from '@chakra-ui/button';
 
 interface PaginationItemProps {
   number: number;
   isCurrent?: boolean;
+  onPageChange: (page: number) => void;
 }
 
-export function PaginationItem({ 
-  isCurrent = false, 
-  number
+export function PaginationItem({
+  isCurrent = false,
+  onPageChange,
+  number,
 }: PaginationItemProps) {
   if (isCurrent) {
     return (
@@ -24,7 +26,7 @@ export function PaginationItem({
       >
         {number}
       </Button>
-    )
+    );
   }
 
   return (
@@ -34,10 +36,11 @@ export function PaginationItem({
       width="4"
       bg="gray.700"
       _hover={{
-        bg: 'gray.500'
+        bg: 'gray.500',
       }}
+      onClick={() => onPageChange(number)}
     >
       {number}
     </Button>
-  )
+  );
 }
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,12 +1,25 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
-import { SubmitHandler, useForm } from 'react-hook-form'
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack,
+} from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup'
-import Link from "next/link";
+import { yupResolver } from '@hookform/resolvers/yup';
+import Link from 'next/link';
 
-import { Input } from "../../components/Form/Input";
-import { Header } from "../../components/Header";
-import { Sidebar } from "../../components/Sidebar";
+import { Input } from '../../components/Form/Input';
+import { Header } from '../../components/Header';
+import { Sidebar } from '../../components/Sidebar';
+import { useMutation } from '@tanstack/react-query';
+import { api } from '../../services/api';
+import { queryClient } from '../../services/queryClient';
+import { useRouter } from 'next/router';
 
 type CreateUserFormData = {
   name: string;
@@ -18,22 +31,50 @@ type CreateUserFormData = {
 const createUserFormSchema = yup.object().shape({
   name: yup.string().required('Nome obrigatório'),
   email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
-  password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
-  password_confirmation: yup.string().oneOf([
-    null, yup.ref('password')
-  ], 'As senhas precisam ser iguais')
-})
+  password: yup
+    .string()
+    .required('Senha obrigatória')
+    .min(6, 'No mínimo 6 caracteres'),
+  password_confirmation: yup
+    .string()
+    .oneOf([null, yup.ref('password')], 'As senhas precisam ser iguais'),
+});
 
 export default function CreateUser() {
-  const { register, handleSubmit, formState, errors } = useForm({
-    resolver: yupResolver(createUserFormSchema)
-  })
+  const router = useRouter();
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post('users', {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
 
-    console.log(values);
-  }
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['users']);
+      },
+    }
+  );
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(createUserFormSchema),
+  });
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await createUser.mutateAsync(values);
+    router.push('/users');
+  };
 
   return (
     <Box>
@@ -47,44 +88,46 @@ export default function CreateUser() {
           flex="1"
           borderRadius={8}
           bg="gray.800"
-          p={["6", "8"]}
+          p={['6', '8']}
           onSubmit={handleSubmit(handleCreateUser)}
         >
-          <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+          <Heading size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
               <Input
                 name="name"
                 label="Nome completo"
                 error={errors.name}
-                ref={register}
+                {...register('name')}
               />
               <Input
                 name="email"
                 type="email"
                 label="E-mail"
                 error={errors.email}
-                ref={register}
+                {...register('email')}
               />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
               <Input
                 name="password"
                 type="password"
                 label="Senha"
                 error={errors.password}
-                ref={register}
+                {...register('password')}
               />
               <Input
                 name="password_confirmation"
                 type="password"
                 label="Confirmação da senha"
                 error={errors.password_confirmation}
-                ref={register}
+                {...register('passwordConfirmation')}
               />
             </SimpleGrid>
           </VStack>
@@ -92,13 +135,11 @@ export default function CreateUser() {
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
               <Link href="/users" passHref>
-                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
               </Link>
-              <Button
-                type="submit"
-                colorScheme="pink"
-                isLoading={formState.isSubmitting}
-              >
+              <Button type="submit" colorScheme="pink" isLoading={isSubmitting}>
                 Salvar
               </Button>
             </HStack>
